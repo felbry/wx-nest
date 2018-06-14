c005332bf9ca79d81b04f3b4ca093e7b52c80702
chaifangbo <chaifangbo@zuoyebang.com>
2018-06-14 19:59:08 +0800

    UPDATE:更新了一些小东西

--- src/api.js
+++ src/api.js
@@ -1,6 +1,20 @@
 import { AV } from '@/storage';
 
+function loading (isStart = true) {
+  isStart ? 
+    wx.showLoading({
+      mask: true
+    })
+    :
+    wx.hideLoading()
+}
+
 // 获取文章列表
 export const Get_Artical = () => {
+  return new AV.Query('Artical').find();
+}
+
+// 新增标签
+export const Set_Artical_Tag = (opt) => {
   return new AV.Query('Artical').find();
 }
